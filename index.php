3159f2400f21868a936200486046401bb17feaa8
tomotPL <89965154+tomotPL@users.noreply.github.com>
2023-11-28 09:03:59 +0100

    Revert "proba formularza"
    This reverts commit 567df364ca8ec2630de4103329deb0a9ba4c8221.

--- index.php
+++ index.php
@@ -10,24 +10,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styl.css">
     <title>Mój Blog</title>
+    <script>
+        function confirmDel() {
+            var userConfirmed = confirm("Czy jesteś pewien, że chcesz usunąć ten wpis?");
+            if (userConfirmed) {
+                document.getElementById("delete").submit();
+            } else {
+                return false;
+            }
+        }
+        function displayInput() {
+            if (document.getElementById('insertform').style.display=='none') {
+                document.getElementById('insertform').style.display='block';
+            } else {
+                document.getElementById('insertform').style.display='none';
+            }
+        }
+    </script>
 </head>
 <body>
     <div class="header">
         <h1>Mój blog
-            <button class="newbut">Dodaj nowy wpis</button>
+            <button class="newbut" onclick="displayInput()">Dodaj nowy wpis</button>
         </h1>
+<div id="insertform" style="display: none;">
+   <h2>Wstawianie </h2>
 
-<h2>Dodaj Blok </h2>
-
-<form method="get" action="index.php">
+<form action="index.php">
   <label for="fname">Nazwa Bloga:</label><br>
   <input type="text" id="fname" name="fname" value=""><br>
   <label for="fname">Treść Bloga:</label><br>
   <input type="text" id="fname" name="fname" value=""><br>
   <input type="submit" value="Przeslij" >
-</form> 
-
-<p>Jezeli dodasz to wyswietlisz temat i Zawartość bloga</p>
+</form>  
+<!--TODO: Działający kod wstawiania-->
+</div>
 
 
 
@@ -38,7 +55,15 @@
 
         <div class="post">
             <?php include('blog_post.php'); ?>
-            <button class = "del">Usuń</button> <!--na pewno musi być z potwierdzeniem poprzez np. alert-->
+            <form id="delete" method="post" onsubmit="return confirmDel()">
+                <button class = "del" name="Delete">Usuń</button>
+            </form>
+            <?php 
+            if (isset($_POST['Delete'])) {
+                //TODO usuń wpis o id $currentPost
+            }
+            ?> 
+            
         </div>
         <?php       
             $newerPost = $currentPost + 1;
@@ -54,36 +79,6 @@
         <p>&copy; 2023 Jan Kowalski</p>
     </div>
 
-    <?php
-$servername = "localhost";
-$username = "username";
-$password = "";
-$dbname = "blok";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-$date_added = $_POST [$date_added];
-$ID= $_POST["ID"];
-$ttile =$_POST ['title'];
-$content = $_POST['content'];
-
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO posts (ID, date_added, title, content, )
-VALUES ('$Title', '$content')";
-
-if (mysqli_query($conn, $sql, )) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-?>
 
 
 </body>
